94228036751a2f3462bda248d8b1bb48cd23d7a4
Tierney Reese <royaldescendants@Tierneys-MacBook-Air.local>
2026-05-20 07:15:50 -0700

    Fixed supabase export

--- app/lib/supabase.ts
+++ app/lib/supabase.ts
@@ -1,11 +1,6 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://qfbpoocvlbxptgebtlvb.supabase.co";
-
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFmYnBvb2N2bGJ4cHRnZWJ0bHZiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkxNTk5NTEsImV4cCI6MjA5NDczNTk1MX0.4nyuYx3tcIm6d0oAh9azrAliRUumf9OrPFqvfiq5Y98";
+import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-  supabaseUrl,
-  supabaseAnonKey
-);
+  'https://qfbpoocvlbxptgebtlvb.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFmYnBvb2N2bGJ4cHRnZWJ0bHZiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkxNTk5NTEsImV4cCI6MjA5NDczNTk1MX0.4nyuYx3tcIm6d0oAh9azrAliRUumf9OrPFqvfiq5Y98'
+)
