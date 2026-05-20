fe8a9b19e715cb3152aad95c554ff39729027a0f
Tierney Reese <royaldescendants@Tierneys-MacBook-Air.local>
2026-05-20 10:45:21 -0700

    Remove Supabase and switched to API form handling

--- app/lib/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-export const supabase = createClient(
-  'https://qfbpoocvlbxptgebtlvb.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFmYnBvb2N2bGJ4cHRnZWJ0bHZiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkxNTk5NTEsImV4cCI6MjA5NDczNTk1MX0.4nyuYx3tcIm6d0oAh9azrAliRUumf9OrPFqvfiq5Y98'
-)
